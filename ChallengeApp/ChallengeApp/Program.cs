c824185819bd01308f918606d944b366a737a79a
DAWIDPRZADKA <daw-prza@wp.pl>
2023-01-29 22:14:19 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,72 @@
-﻿var name = "Maria";
-var sex = 'F';
-var age = 29;
+﻿using System.Diagnostics.Metrics;
 
-if (sex == 'F' && age < 30)
+int number = 232534;
+string numberAsString = number.ToString();
+char[] Letters = numberAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char Letter in Letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (Letter == '0')
+    {
+        counter0++;
+    }
+    else if (Letter == '1')
+    {
+        counter1++;
+    }
+    else if (Letter == '2')
+    {
+        counter2++;
+    }
+    else if (Letter == '3')
+    {
+        counter3++;
+    }
+    else if (Letter == '4')
+    {
+        counter4++;
+    }
+    else if (Letter == '5')
+    {
+        counter5++;
+    }
+    else if (Letter == '6')
+    {
+        counter6++;
+    }
+    else if (Letter == '7')
+    {
+        counter7++;
+    }
+    else if (Letter == '8')
+    {
+        counter8++;
+    }
+    else if (Letter == '9')
+    {
+        counter9++;
+    }
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+        {
+            Console.WriteLine("0 => " + (counter0));
+            Console.WriteLine("1 => " + (counter1));
+            Console.WriteLine("2 => " + (counter2));
+            Console.WriteLine("3 => " + (counter3));
+            Console.WriteLine("4 => " + (counter4));
+            Console.WriteLine("5 => " + (counter5));
+            Console.WriteLine("6 => " + (counter6));
+            Console.WriteLine("7 => " + (counter7));
+            Console.WriteLine("8 => " + (counter8));
+            Console.WriteLine("9 => " + (counter9));
 }
